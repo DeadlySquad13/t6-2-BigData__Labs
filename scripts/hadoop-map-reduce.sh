8666f02e432406dd2faec909cd761737722f2e0b
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-04-28 18:58:21 +0000

    [Style](Scripts)

--- scripts/hadoop-map-reduce.sh
+++ scripts/hadoop-map-reduce.sh
@@ -9,18 +9,18 @@ hdfs dfs -rm -r "$OUTPUT_DIR/*"
 
 rows=( 100 1000 10000 100000 1000000 2000000 3000000 4000000 5000000 )
 for i in "${rows[@]}"; do
- SECONDS=0
+  SECONDS=0
 
- INPUT_NAME="test-$i"
+  INPUT_NAME="test-$i"
 
- hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar \
-  -mapper $HOME/scripts/mapper.py \
-  -reducer $HOME/scripts/reducer.py \
-  -input "$INPUT_DIR/$INPUT_NAME.csv" \
-  -output "$OUTPUT_DIR/$INPUT_NAME" \
-  
- elapsedseconds=$SECONDS
- echo "Elapsed: $elapsedseconds"
+  hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar \
+   -mapper $HOME/scripts/mapper.py \
+   -reducer $HOME/scripts/reducer.py \
+   -input "$INPUT_DIR/$INPUT_NAME.csv" \
+   -output "$OUTPUT_DIR/$INPUT_NAME" \
+   
+  elapsedseconds=$SECONDS
+  echo "Elapsed: $elapsedseconds"
 
- hdfs dfs -tail "$OUTPUT_DIR/$INPUT_NAME/part-00000";
+  hdfs dfs -tail "$OUTPUT_DIR/$INPUT_NAME/part-00000";
 done
